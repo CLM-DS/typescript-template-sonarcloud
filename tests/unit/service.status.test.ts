e663fb2908fa16dc4e86d698b4b435dcc38adf8f
Fernando Ochoa <fochoa@clmconsultores.com>
2021-06-04 20:28:49 -0400

    Update service status tests

--- tests/unit/service.status.test.ts
+++ tests/unit/service.status.test.ts
@@ -2,7 +2,10 @@ import { httpStatus } from '../../app/constants';
 import { statusService } from '../../app/services';
 import { createMockContext } from '@shopify/jest-koa-mocks';
 import { createLogger } from '../../app/utils/logger';
-import { createMockConfig } from '../mocks';
+import { createMockConfig, createMockPool } from '../mocks';
+import * as db from '../../app/utils/wrapperDB';
+
+jest.mock('../../app/utils/wrapperDB');
 
 describe('Test Cases: Status Service', () => {
   it('Test Status Healthy Success', () => {
@@ -20,6 +23,7 @@ describe('Test Cases: Status Service', () => {
 
   it('Test Status Alive Success', () => {
     const ctxMock = createMockContext();
+    ctxMock.config = { brokerConfig: {} };
     const res = statusService.alive(ctxMock);
     expect(res.status).toEqual(httpStatus.statusCodes.OK);
   });
@@ -27,6 +31,31 @@ describe('Test Cases: Status Service', () => {
   it('Test Status Alive Failure', () => {
     const ctxMock = createMockContext();
     ctxMock.config = createMockConfig; 
+    const res = statusService.alive(ctxMock);
+    expect(res.status).toEqual(httpStatus.statusCodes.SERVICE_UNAVAILABLE);
+  });
+
+  it('Test Status Alive Success with DB', () => {
+    const ctxMock = createMockContext();
+    const dbMock = db as jest.Mocked<typeof db>;
+    dbMock.isConnected.mockReturnValueOnce(true);
+    ctxMock.db = dbMock;
+    ctxMock.config = createMockConfig;
+    const res = statusService.alive(ctxMock);
+    expect(res.status).toEqual(httpStatus.statusCodes.OK);
+  });
+
+  it('Test Status Alive Success with Pool', () => {
+    const ctxMock = createMockContext();
+    ctxMock.pool = createMockPool(true);
+    ctxMock.config = { brokerConfig: { kafkaConsumer: { type: 'kafka' } } };
+    const res = statusService.alive(ctxMock);
+    expect(res.status).toEqual(httpStatus.statusCodes.OK);
+  });
+
+  it('Test Status Alive Failure with no Pool', () => {
+    const ctxMock = createMockContext();
+    ctxMock.config = { brokerConfig: { kafkaConsumer: { type: 'kafka' } } };
     const res = statusService.alive(ctxMock);
     expect(res.status).toEqual(httpStatus.statusCodes.SERVICE_UNAVAILABLE);
   });

b6abc503f11b5f001ef30e614ee37ce766cb0687
Fernando Ochoa <fochoa@clmconsultores.com>
2021-06-08 13:31:47 -0400

    Add unit tests for the utils/validator file

---
+++ tests/unit/utils.validator.test.ts
@@ -0,0 +1,84 @@
+import joi from 'joi';
+import { httpStatus } from '../../app/constants';
+import { useValidation } from '../../app/utils/validator';
+import { createMockContext } from '@shopify/jest-koa-mocks';
+import { createLogger } from '../../app/utils/logger';
+import { Context } from 'koa';
+import { createMockRequest } from '../mocks';
+
+describe('Test Cases: validator', () => {
+  it('Test schema validation failure', () => {
+    const ctxMock = createMockContext();
+    ctxMock.log = createLogger();
+    const scheme = joi.object({
+      orderId: joi.required(),
+    });
+    const validation = useValidation([{
+      property: 'request.body',
+      scheme,
+    }], (c) => c)(ctxMock);
+    
+    expect(ctxMock.status).toEqual(httpStatus.statusCodes.BAD_REQUEST);
+    expect((validation as Context).response.body).toEqual({'message': 'Data not found', 'property': 'request.body'});
+  });
+
+  it('Test schema validation success', () => {
+    const ctxMock = createMockContext({...createMockRequest});
+    ctxMock.log = createLogger();
+    const scheme = joi.object({
+      orderId: joi.required(),
+    });
+    const validation = useValidation([{
+      property: 'request.body',
+      scheme,
+    }], (ctx) => {
+      ctx.status = httpStatus.statusCodes.OK;
+      return ctx;
+    })(ctxMock);
+
+    expect(ctxMock.status).toEqual(httpStatus.statusCodes.OK);
+    expect((validation as Context).response.body).toBeUndefined();
+  });
+
+  it('Test schema validation failure on body property', () => {
+    const ctxMock = createMockContext({...createMockRequest});
+    ctxMock.log = createLogger();
+    const scheme = joi.object({
+      orderId: joi.required(),
+    });
+    const validation = useValidation([{
+      property: 'request.body',
+      scheme,
+    },
+    {
+      property: 'body',
+      scheme,
+    }], (ctx) => {
+      ctx.status = httpStatus.statusCodes.OK;
+      return ctx;
+    })(ctxMock);
+    expect(ctxMock.status).toEqual(httpStatus.statusCodes.BAD_REQUEST);
+    expect((validation as Context).response.body).toEqual({'message': 'Data not found', 'property': 'body'});
+  });
+
+  it('Test schema validation failure on request.body property', () => {
+    const ctxMock = createMockContext();
+    ctxMock.log = createLogger();
+    const scheme = joi.object({
+      orderId: joi.required(),
+    });
+    const validation = useValidation([{
+      property: 'request.body',
+      scheme,
+    },
+    {
+      property: 'body',
+      scheme,
+    }], (ctx) => {
+      ctx.status = httpStatus.statusCodes.OK;
+      return ctx;
+    })(ctxMock);
+    expect(ctxMock.status).toEqual(httpStatus.statusCodes.BAD_REQUEST);
+    expect((validation as Context).response.body).toEqual({'message': 'Data not found', 'property': 'request.body'});
+  });
+});

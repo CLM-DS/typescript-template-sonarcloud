83bf942daa08f9afc949a94c7d11a85f2d8da35c
Fernando Ochoa <fochoa@clmconsultores.com>
2021-06-22 14:30:15 -0400

    Update uuid package used to generate eventIds

--- app/server/middlewares/interceptorMiddleware.ts
+++ app/server/middlewares/interceptorMiddleware.ts
@@ -1,12 +1,12 @@
 import { Context, Next } from 'koa';
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * Intercept request and add information.
  * @returns {(ctx: import('.').ContextStd, next: import('koa').Next) => import('koa')}
  */
 const interceptorMiddleware = () => async (ctx: Context, next: Next): Promise<Context> => {
-  ctx.request.header['x-txref'] = ctx.request.header['x-txref'] || uuid();
+  ctx.request.header['x-txref'] ||= uuidv4();
   await next();
   return ctx;
 };
